ea3137b957e0e3deca336e0087522d2c7221c8c0
David Mihola <david.mihola@sperling.cz>
2023-02-28 13:01:20 +0100

    master-cleanup

--- Sudoku/sprint.py
+++ Sudoku/sprint.py
@@ -9,8 +9,7 @@ def check_sudoku(grid):
     for i in range(9):
         # j, k index top left hand corner of each 3x3 tile
         j, k = (i // 3) * 3, (i % 3) * 3
-        if len(set(grid[i,:])) != 9 or len(set(grid[:,i])) != 9\
-                   or len(set(grid[j:j+3, k:k+3].ravel())) != 9:
+        if len(set(grid[i,:])) != 9 or len(set(grid[:,i])) != 9 or len(set(grid[j:j+3, k:k+3].ravel())) != 9:
             return False
     return True
 
